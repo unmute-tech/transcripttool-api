adce09d18284204b8762880f0c537078725cb0fa
Thomas Reitmaier <thomas@reitmaier.xyz>
2022-01-30 13:31:00 +0000

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val coroutines_version: String by project
+val postgres_version: String by project
+val sqldelight_version: String by project
+val hikaricp_version: String by project
 
 plugins {
   application
   kotlin("jvm") version "1.6.10"
   id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+
+  // DB
+  id("com.squareup.sqldelight") version "1.5.3"
 }
 
 group = "xyz.reitmaier"
@@ -20,6 +27,7 @@ repositories {
 }
 
 dependencies {
+  // ktor
   implementation("io.ktor:ktor-server-core:$ktor_version")
   implementation("io.ktor:ktor-server-auth:$ktor_version")
   implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
@@ -27,7 +35,31 @@ dependencies {
   implementation("io.ktor:ktor-server-partial-content:$ktor_version")
   implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
   implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+  // db -- config
+  implementation("com.zaxxer:HikariCP:$hikaricp_version")
+
+  // db -- postgres
+  implementation("org.postgresql:postgresql:$postgres_version")
+
+  // db -- sqldelight
+  implementation("com.squareup.sqldelight:runtime-jvm:$sqldelight_version")
+  implementation("com.squareup.sqldelight:jdbc-driver:$sqldelight_version")
+  implementation("com.squareup.sqldelight:coroutines-extensions:$sqldelight_version")
+
+  // logging
   implementation("ch.qos.logback:logback-classic:$logback_version")
+  implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger:1.0.4")
+
+  // Coroutines
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+
+
+  // Result monad for modelling success/failure operations
+  implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.14")
+  implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:1.1.14")
+
+  // testing
   testImplementation("io.ktor:ktor-server-tests:$ktor_version")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
